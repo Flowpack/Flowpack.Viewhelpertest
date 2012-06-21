d22494da139ac95959470e128fc87971f4345836
Bastian Waidelich <bastian@typo3.org>
2012-06-21 12:24:48 +0200

    [FEATURE] Collapse succesful test cases by default
    This adds a "results" section below the view helper tests
    displaying the number of succesful & failed tests with a
    link that allows to collapse/expand successful test results.
    
    Change-Id: I199800798d5cad9503677148bf0ddbdf012fc44d

--- Resources/Public/javascript.js
+++ Resources/Public/javascript.js
@@ -1,3 +1,24 @@
 Ext.onReady(function() {
 	Ext.QuickTips.init();
+	var results = Ext.get('results');
+	if (results != null) {
+		var successfulTests = Ext.select('.viewhelpertest .testcase.success');
+		var failedTests = Ext.select('.viewhelpertest .testcase.failure');
+		successfulTests.setVisibilityMode(Ext.Element.DISPLAY);
+		Ext.select('.viewhelpertest .testcase.success').hide();
+		var collapsed = true;
+		results.update('Tests: ' + (successfulTests.elements.length + failedTests.elements.length) + ', <span class="success">Successful: ' + successfulTests.elements.length + '</span>, <span class="failure">Failures: ' + failedTests.elements.length + '</span> - ');
+		Ext.DomHelper.append('results', {tag: 'a', href: '#', html: 'expand all', id: 'expandLink'});
+		Ext.fly('expandLink').on('click', function(e) {
+			e.preventDefault();
+			if (collapsed) {
+				successfulTests.show();
+				this.update('collapse succesful');
+			} else {
+				successfulTests.hide();
+				this.update('expand all');
+			}
+			collapsed = !collapsed;
+		});
+	}
 });
