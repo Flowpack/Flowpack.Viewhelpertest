69a281ea25ebfe58c692276ae5ed58ac29306849
Bastian Waidelich <bastian@typo3.org>
2013-05-09 16:36:44 +0200

    [TASK] Fix package key in doc comments
    Change-Id: Ic3aefaf42a1e4901db9b1ce8a405ef6467e37414

--- Classes/TYPO3/Viewhelpertest/SomeSubpackage/Controller/SomeController.php
+++ Classes/TYPO3/Viewhelpertest/SomeSubpackage/Controller/SomeController.php
@@ -2,7 +2,7 @@
 namespace TYPO3\Viewhelpertest\SomeSubpackage\Controller;
 
 /*                                                                        *
- * This script belongs to the FLOW3 package "Viewhelpertest".             *
+ * This script belongs to the FLOW3 package "TYPO3.Viewhelpertest".       *
  *                                                                        *
  * It is free software; you can redistribute it and/or modify it under    *
  * the terms of the GNU Lesser General Public License, either version 3   *
